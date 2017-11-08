ef81d927a6b7dea3287453e20ff4a62cc87a25f0
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-08 15:54:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,6 @@ class Song
     sql = "pragma table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
-    binding.pry
     column_names = []
     table_info.each do |row|
       column_names << row["name"]
@@ -61,5 +60,5 @@ class Song
 
 end
 
-class Artist < Song
-end
+
+
